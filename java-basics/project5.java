3b6dfbccf94d9d6435d4840f630d6d1aaa5a9c41
Ira Jade Hingada <irahingada@gmail.com>
2025-06-24 21:07:39 +0800

    Create project5.java

---
+++ java-basics/project5.java
@@ -0,0 +1,19 @@
+package irapractice;
+
+public class project5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+	}
+
+	public String getuserData()
+	{
+		System.out.println("hello world");
+	     return "ira jade hingada";
+	}
+}
+
+//magkadugtong sa project 4
+
+# Support class for method calling in project4 #
